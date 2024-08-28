797f6811b6ace01e9a535be380588c186a0eb8a0
sushantsharma-coco <cocosingh456@gmail.com>
2024-08-28 17:51:50 +0530

    end

---
+++ slot-machine.js
@@ -0,0 +1,98 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var ASlotMachine = /** @class */ (function () {
+    function ASlotMachine() {
+        this.rtpRates = {
+            jackpot: 50,
+            threeOfKind: 10,
+            twoOfKind: 2,
+        };
+        this.profit = 0;
+        this.totalWinCount = 0;
+        this.jackpotCount = 0;
+        this.threeOfKindCount = 0;
+        this.twoOfKindCount = 0;
+        this.playedCount = 0;
+        this.investAmt = 0;
+        this.returnAmt = 0;
+        //   abstract rtpEvaluator(): number;
+    }
+    return ASlotMachine;
+}());
+var SlotMachine = /** @class */ (function (_super) {
+    __extends(SlotMachine, _super);
+    function SlotMachine(spinRange, potAmt, betAmt) {
+        var _this = _super.call(this) || this;
+        _this.spinRange = spinRange;
+        _this.potAmt = potAmt;
+        _this.betAmt = betAmt;
+        var result;
+        for (var i = 0; i < _this.spinRange; i++) {
+            result = _this.spin();
+        }
+        // @ts-ignore
+        var totalInvestment = result.totalInvestment, returnAmt = result.returnAmt;
+        var nresult = _this.houseProfit(totalInvestment, returnAmt);
+        // @ts-ignore
+        result.companyProfit = nresult;
+        // @ts-ignore
+        console.log(result);
+        return _this;
+    }
+    SlotMachine.prototype.randomValueGenerator = function () {
+        return Math.floor(Math.random() * 10);
+    };
+    SlotMachine.prototype.spin = function () {
+        var v1 = this.randomValueGenerator();
+        var v2 = this.randomValueGenerator();
+        var v3 = this.randomValueGenerator();
+        this.investAmt += this.betAmt;
+        this.playedCount++;
+        if (v1 === v2 && v2 === v3 && v1 == 7) {
+            /*jackpot 777*/
+            this.jackpotCount++;
+            this.totalWinCount++;
+            this.returnAmt += this.betAmt * this.rtpRates.jackpot;
+        }
+        else if (v1 === v2 && v2 === v3 && v1 !== 7) {
+            /*threeOfKind 111,222...,!777*/
+            this.threeOfKindCount++;
+            this.totalWinCount++;
+            this.returnAmt += this.betAmt * this.rtpRates.threeOfKind;
+        }
+        else if (v1 === v2 || v2 === v3 || v3 === v1) {
+            /*twoOfKind 112,221,121,212...*/
+            this.totalWinCount++;
+            this.twoOfKindCount++;
+            this.returnAmt += this.betAmt * this.rtpRates.twoOfKind;
+        }
+        return {
+            timesPlayed: this.playedCount,
+            jackpot: this.jackpotCount,
+            threeOfKindCount: this.threeOfKindCount,
+            twoOfKindCount: this.twoOfKindCount,
+            totalWinCount: this.totalWinCount,
+            totalInvestment: this.investAmt,
+            returnAmt: this.returnAmt,
+            companyProfit: this.profit,
+        };
+    };
+    SlotMachine.prototype.houseProfit = function (invst, rtrn) {
+        return invst - rtrn;
+    };
+    return SlotMachine;
+}(ASlotMachine));
+var slot = new SlotMachine(1000000, 100, 10);
---
+++ slot-machine.ts
@@ -0,0 +1,122 @@
+interface IRtp {
+  jackpot: number;
+  threeOfKind: number;
+  twoOfKind: number;
+}
+
+interface ISpinResult {
+  timesPlayed: number;
+  jackpot: number;
+  threeOfKindCount: number;
+  twoOfKindCount: number;
+  totalWinCount: number;
+  totalInvestment: number;
+  returnAmt: number;
+  companyProfit: number;
+}
+
+abstract class ASlotMachine {
+  rtpRates: IRtp = {
+    jackpot: 50,
+    threeOfKind: 10,
+    twoOfKind: 2,
+  };
+
+  protected profit: number = 0;
+
+  protected spinRange: number;
+  protected potAmt: number;
+  protected betAmt: number;
+
+  protected totalWinCount: number = 0;
+  protected jackpotCount: number = 0;
+  protected threeOfKindCount: number = 0;
+  protected twoOfKindCount: number = 0;
+
+  protected playedCount: number = 0;
+  protected investAmt: number = 0;
+  protected returnAmt: number = 0;
+
+  abstract randomValueGenerator(): number;
+  abstract spin(): object;
+  abstract houseProfit(x: number, y: number): number;
+  //   abstract rtpEvaluator(): number;
+}
+
+class SlotMachine extends ASlotMachine {
+  constructor(spinRange: number, potAmt: number, betAmt: number) {
+    super();
+
+    this.spinRange = spinRange;
+    this.potAmt = potAmt;
+    this.betAmt = betAmt;
+
+    let result: ISpinResult;
+    for (let i = 0; i < this.spinRange; i++) {
+      result = this.spin();
+    }
+    // @ts-ignore
+    let { totalInvestment, returnAmt } = result;
+
+    let nresult = this.houseProfit(totalInvestment, returnAmt);
+
+    // @ts-ignore
+    result.companyProfit = nresult;
+    // @ts-ignore
+
+    console.log(result);
+  }
+
+  randomValueGenerator(): number {
+    return Math.floor(Math.random() * 10);
+  }
+
+  spin(): ISpinResult {
+    let v1: number = this.randomValueGenerator();
+    let v2: number = this.randomValueGenerator();
+    let v3: number = this.randomValueGenerator();
+
+    this.investAmt += this.betAmt;
+    this.playedCount++;
+
+    if (v1 === v2 && v2 === v3 && v1 == 7) {
+      /*jackpot 777*/
+
+      this.jackpotCount++;
+      this.totalWinCount++;
+
+      this.returnAmt += this.betAmt * this.rtpRates.jackpot;
+    } else if (v1 === v2 && v2 === v3 && v1 !== 7) {
+      /*threeOfKind 111,222...,!777*/
+
+      this.threeOfKindCount++;
+      this.totalWinCount++;
+
+      this.returnAmt += this.betAmt * this.rtpRates.threeOfKind;
+    } else if (v1 === v2 || v2 === v3 || v3 === v1) {
+      /*twoOfKind 112,221,121,212...*/
+
+      this.totalWinCount++;
+      this.twoOfKindCount++;
+
+      this.returnAmt += this.betAmt * this.rtpRates.twoOfKind;
+    }
+
+    return {
+      timesPlayed: this.playedCount,
+      jackpot: this.jackpotCount,
+      threeOfKindCount: this.threeOfKindCount,
+      twoOfKindCount: this.twoOfKindCount,
+      totalWinCount: this.totalWinCount,
+      totalInvestment: this.investAmt,
+      returnAmt: this.returnAmt,
+      companyProfit: this.profit,
+    };
+  }
+
+  houseProfit(invst: number, rtrn: number): number {
+    return invst - rtrn;
+  }
+}
+
+let slot = new SlotMachine(1000000, 100, 10);
